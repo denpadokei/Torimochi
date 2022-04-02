3de535ce57cd8814dbc8fc3c52d0b19747103c0a
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-04-02 16:18:19 +0900

    プロジェクト ファイルを追加します。

---
+++ Torimochi/Models/ObjectMemoryPool.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Torimochi.Models
+{
+    public class ObjectMemoryPool<T> : IDisposable where T : Component
+    {
+        private readonly ConcurrentQueue<T> _freeObjects;
+        private readonly Action<T> _firstAlloc;
+        private readonly Action<T> _onAlloc;
+        private readonly Action<T> _onFree;
+        private readonly Func<T> _constructor;
+        private bool _disposedValue;
+
+        /// <summary>
+        /// ObjectPool constructor function, used to setup the initial pool size and callbacks.
+        /// </summary>
+        /// <param name="initialCount">The number of components of type T to allocate right away.</param>
+        /// <param name="constructor">The constructor function used to create new objects in the pool.</param>
+        /// <param name="firstAlloc">The callback function you want to occur only the first time when a new component of type T is allocated.</param>
+        /// <param name="onAlloc">The callback function to be called everytime ObjectPool.Alloc() is called.</param>
+        /// <param name="onFree">The callback function to be called everytime ObjectPool.Free() is called</param>
+        public ObjectMemoryPool(int initialCount = 0, Func<T> constructor = null, Action<T> firstAlloc = null, Action<T> onAlloc = null, Action<T> onFree = null)
+        {
+            this._constructor = constructor;
+            this._firstAlloc = firstAlloc;
+            this._onAlloc = onAlloc;
+            this._onFree = onFree;
+            this._freeObjects = new ConcurrentQueue<T>();
+
+            for (var i = 0; i < initialCount; i++) {
+                this._freeObjects.Enqueue(this.InternalAlloc());
+            }
+        }
+
+        private T InternalAlloc()
+        {
+            var newObj = this._constructor is null ? new GameObject("New Component Object", typeof(T)).GetComponent<T>() : this._constructor.Invoke();
+            this._firstAlloc?.Invoke(newObj);
+            return newObj;
+        }
+
+        /// <summary>
+        /// Allocates a component of type T from a pre-allocated pool, or instantiates a new one if required.
+        /// </summary>
+        /// <returns></returns>
+        public T Alloc()
+        {
+            if (!this._freeObjects.TryDequeue(out var obj) || obj == null) {
+                obj = this.InternalAlloc();
+            }
+            this._onAlloc?.Invoke(obj);
+            return obj;
+        }
+
+        /// <summary>
+        /// Inserts a component of type T into the stack of free objects. Note: the component does *not* need to be allocated using ObjectPool.Alloc() to be freed with this function!
+        /// </summary>
+        /// <param name="obj"></param>
+        public void Free(T obj)
+        {
+            if (obj == null) {
+                return;
+            }
+            this._onFree?.Invoke(obj);
+            this._freeObjects.Enqueue(obj);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposedValue) {
+                if (disposing) {
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                    while (this._freeObjects.TryDequeue(out var obj) && obj != null) {
+                        GameObject.Destroy(obj.gameObject);
+                    }
+                    this._freeObjects.Clear();
+                }
+                // TODO: アンマネージド リソース (アンマネージド オブジェクト) を解放し、ファイナライザーをオーバーライドします
+                // TODO: 大きなフィールドを null に設定します
+                this._disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Torimochi/TorimochiController.cs
@@ -0,0 +1,201 @@
+﻿using IPA.Utilities;
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Torimochi.Models;
+using UnityEngine;
+using Zenject;
+
+namespace Torimochi
+{
+    /// <summary>
+    /// Monobehaviours (scripts) are added to GameObjects.
+    /// For a full list of Messages a Monobehaviour can receive from the game, see https://docs.unity3d.com/ScriptReference/MonoBehaviour.html.
+    /// </summary>
+    public class TorimochiController : IInitializable, IDisposable
+    {
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // プロパティ
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // コマンド
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // コマンド用メソッド
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // オーバーライドメソッド
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // パブリックメソッド
+        public void Initialize()
+        {
+            this._beatmapObjectManager.noteWasCutEvent += this.OnNoteWasCutEvent;
+            var noteController = Resources.FindObjectsOfTypeAll<GameNoteController>().FirstOrDefault();
+            this._noteMeshPool = new ObjectMemoryPool<MeshRenderer>(100,
+                () =>
+                {
+                    Logger.Debug("constractor");
+                    var go = new GameObject("CloneNoteMesh");
+                    var meshClone = GameObject.Instantiate(this._noteMeshRendrer);
+                    meshClone.transform.SetParent(go.transform);
+                    meshClone.transform.localPosition = Vector3.zero;
+                    meshClone.transform.localRotation = Quaternion.identity;
+                    meshClone.enabled = true;
+                    foreach (var item in go.GetComponentsInChildren<BoxCollider>(true)) {
+                        GameObject.Destroy(item);
+                    }
+                    return meshClone;
+                },
+                null,
+                mesh =>
+                {
+                    Logger.Debug("alloc");
+                    Logger.Debug($"alloc activecount : {this._activeNoteCount}");
+                    foreach (var target in mesh.gameObject.GetComponentsInChildren<MeshRenderer>()) {
+                        target.enabled = true;
+                        target.forceRenderingOff = false;
+                    }
+                    this._activeMesh.Enqueue(mesh);
+                    this._activeNoteCount++;
+                },
+                mesh =>
+                {
+                    Logger.Debug("free");
+                    Logger.Debug($"free activecount : {this._activeNoteCount}");
+                    foreach (var target in mesh.gameObject.GetComponentsInChildren<MeshRenderer>()) {
+                        target.enabled = false;
+                        target.forceRenderingOff = true;
+                    }
+                    this._activeNoteCount--;
+                });
+        }
+
+        private void OnNoteWasCutEvent(NoteController noteController, in NoteCutInfo noteCutInfo)
+        {
+            if (!noteCutInfo.allIsOK) {
+                return;
+            }
+            try {
+                var noteMesh = this._noteMeshPool.Alloc();
+                switch (noteCutInfo.saberType) {
+                    case SaberType.SaberA:
+                        var leftSaber = this._saberManager.leftSaber;
+                        noteMesh.gameObject.transform.SetParent(leftSaber.transform, false);
+                        break;
+                    case SaberType.SaberB:
+                        var rightSaber = this._saberManager.rightSaber;
+                        noteMesh.gameObject.transform.SetParent(rightSaber.transform, false);
+                        break;
+                    default:
+                        break;
+                }
+                var color = this._colorManager.ColorForType(noteController.noteData.colorType);
+                var isAny = false;
+                switch (noteCutInfo.noteData.cutDirection) {
+                    case NoteCutDirection.Any:
+                        isAny = true;
+                        break;
+                    case NoteCutDirection.Up:
+                    case NoteCutDirection.Down:
+                    case NoteCutDirection.Left:
+                    case NoteCutDirection.Right:
+                    case NoteCutDirection.UpLeft:
+                    case NoteCutDirection.UpRight:
+                    case NoteCutDirection.DownLeft:
+                    case NoteCutDirection.DownRight:
+                    case NoteCutDirection.None:
+                    default:
+                        isAny = false;
+                        break;
+                }
+                var arrow = noteMesh.gameObject.GetComponentsInChildren<MeshRenderer>().FirstOrDefault(x => x.name == s_arrowName);
+                var arrowGlow = noteMesh.gameObject.GetComponentsInChildren<MeshRenderer>().FirstOrDefault(x => x.name == "NoteArrowGlow");
+                var circleGlow = noteMesh.gameObject.GetComponentsInChildren<MeshRenderer>().FirstOrDefault(x => x.name == "NoteCircleGlow");
+                arrow.forceRenderingOff = isAny;
+                arrowGlow.forceRenderingOff = isAny;
+                circleGlow.forceRenderingOff = !isAny;
+                foreach (var target in noteMesh.gameObject.GetComponentsInChildren<MaterialPropertyBlockController>()) {
+                    var prop = target.materialPropertyBlock;
+                    if (target.name == s_arrowName) {
+                        prop.SetColor(s_colorId, Color.white.ColorWithAlpha(1f));
+                    }
+                    else {
+                        prop.SetColor(s_colorId, color.ColorWithAlpha(1f));
+                    }
+                    target.ApplyChanges();
+                }
+                noteMesh.transform.position = noteCutInfo.notePosition;
+                noteMesh.transform.rotation = noteCutInfo.noteRotation;
+                while (99 < this._activeNoteCount && this._activeMesh.TryDequeue(out var oldActiveNote)) {
+                    Logger.Debug($"free mesh : {this._activeNoteCount}");
+                    oldActiveNote.gameObject.transform.SetParent(null);
+                    this._noteMeshPool.Free(oldActiveNote);
+                }
+            }
+            catch (Exception e) {
+                Plugin.Log.Error(e);
+            }
+        }
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // プライベートメソッド
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // メンバ変数
+        private SaberManager _saberManager;
+        private BeatmapObjectManager _beatmapObjectManager;
+        protected ColorManager _colorManager;
+        private ObjectMemoryPool<MeshRenderer> _noteMeshPool;
+        private ConcurrentQueue<MeshRenderer> _activeMesh = new ConcurrentQueue<MeshRenderer>();
+        private MeshRenderer _noteMeshRendrer;
+        private bool _disposedValue;
+        int _activeNoteCount = 0;
+        private static readonly int s_colorId = Shader.PropertyToID("_Color");
+        private static readonly string s_arrowName = "NoteArrow";
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+        #region // 構築・破棄
+        [Inject]
+        public TorimochiController(SaberManager saberManager, BeatmapObjectManager beatmapObjectManager, ColorManager colorManager, [Inject(Id = NoteData.GameplayType.Normal)] GameNoteController.Pool basicGameNotePool)
+        {
+            this._saberManager = saberManager;
+            this._beatmapObjectManager = beatmapObjectManager;
+            this._colorManager = colorManager;
+            // 絶対なんかよろしくない気がする。
+            var note = basicGameNotePool.Spawn();
+            var disappearingArrowController = note.gameObject.GetComponentInParent<DisappearingArrowController>();
+            var noteMesh = disappearingArrowController.GetField<MeshRenderer, DisappearingArrowControllerBase<GameNoteController>>("_cubeMeshRenderer");
+            basicGameNotePool.Despawn(note);
+            this._noteMeshRendrer = GameObject.Instantiate(noteMesh);
+            this._noteMeshRendrer.transform.SetParent(null);
+            this._noteMeshRendrer.enabled = false;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue) {
+                if (disposing) {
+                    // TODO: マネージド状態を破棄します (マネージド オブジェクト)
+                    GameObject.Destroy(this._noteMeshRendrer);
+                    this._noteMeshPool.Dispose();
+                    this._beatmapObjectManager.noteWasCutEvent -= this.OnNoteWasCutEvent;
+                }
+                _disposedValue = true;
+            }
+        }
+        public void Dispose()
+        {
+            // このコードを変更しないでください。クリーンアップ コードを 'Dispose(bool disposing)' メソッドに記述します
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+        //ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
+    }
+}
